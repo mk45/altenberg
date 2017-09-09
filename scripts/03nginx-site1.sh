104394dc7875264aa52da4693b116b881d8fd8fd
maciek <maciej.kaminski@pwr.edu.pl>
2017-09-09 23:10:35 +0200

    Cache working

---
+++ scripts/03nginx-site1.sh
@@ -0,0 +1,6 @@
+if [ ! -f "$0.done" ]; then
+    cp altenberg /etc/nginx/sites-available/altenberg
+    cp welcome /etc/nginx/sites-available/welcome
+    systemctl restart nginx
+    touch "$0.done"
+fi`
